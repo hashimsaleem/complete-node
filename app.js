114db5bac5d87338fa3e0716a4d599eec52e4cb2
Hash Soloman <hashim.saleem@gmail.com>
2018-03-22 23:32:08 +0500

    JSON

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 
 console.log('Starting app.js');
 
-console.log(process.argv);
+const yargs = require('yargs');
+const notes = require('./notes');
+
+const argv = yargs.argv;
+let command = argv._[0];
+
+if(command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if(command === 'remove') {
+    notes.removeNote(argv.title);
+} else if(command === 'list') {
+    notes.getAll();
+} else if(command === 'read') {
+    notes.getNote(argv.title);
+} else {
+    console.log('unknown command');
+}
---
+++ playground/json.js
@@ -0,0 +1,8 @@
+let obj = {
+    name: 'Hashim'
+};
+
+let stringObj = JSON.stringify(obj);
+console.log(typeof (stringObj));
+
+console.log(stringObj);
