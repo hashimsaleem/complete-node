0117d3d213fce9316eaf70460d0bfa6f4f65a413
Hash Soloman <hashim.saleem@gmail.com>
2018-03-23 14:29:06 +0500

    add save note refactored

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const argv = yargs.argv;
 let command = argv._[0];
 
 if(command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    let note = notes.addNote(argv.title, argv.body);
+    if(note) {
+        console.log('Note created');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Note title taken');
+    }
 } else if(command === 'remove') {
     notes.removeNote(argv.title);
 } else if(command === 'list') {
--- notes.js
+++ notes.js
@@ -1,7 +1,34 @@
 console.log('starting notes.js');
 
+const fs = require('fs');
+
+let fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+let saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 let addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    let notes = fetchNotes();
+    
+    let duplicateNotes = notes.filter((note) => note.title === title);
+    if (duplicateNotes.length === 0) {
+        let note = {
+            title,
+            body
+        };
+    
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
 
 let getAll = () => {
